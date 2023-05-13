45593edf36b391aa57f16522e493b3643b0d0d43
Fellyipe <felipebueno2004@hotmail.com>
2023-05-13 20:40:33 -0300

    sasasad

--- program.cs
+++ program.cs
@@ -1,1 +1,24 @@
-aaaaaa
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* ------------- ALUNOS -------------------
+ÍSIS YASMIM
+GUILHERME FAVERO
+FELIPE BUENO
+*/
+using Gamificacao3;
+
+
+namespace Gamificacao2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+
