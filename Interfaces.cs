c9453ef1e810da168f715df3babfeb3e8cab9b29
Fellyipe <felipebueno2004@hotmail.com>
2023-05-13 21:56:50 -0300

    asdsad

--- Interfaces.cs
+++ Interfaces.cs
@@ -4,24 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Pedido
-{
-    public int Id { get; set; }
-    public DateTime Data { get; set; }
-    public string Cliente { get; set; }
-    public string Status { get; set; }
-    public List<ItemPedido> Itens { get; set; }
-}
-
-public class ItemPedido
-{
-    public int Id { get; set; }
-    public Produto Produto { get; set; }
-    public int Quantidade { get; set; }
-    public decimal PrecoUnitario { get; set; }
-    public Pedido Pedido { get; set; }
-}
-
 // Interface de repositório
 public interface IPedidoRepository
 {
