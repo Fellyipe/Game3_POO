76cc4ba05f64a59f8ce526dac47b17d2198b6e07
Fellyipe <felipebueno2004@hotmail.com>
2023-05-16 13:46:21 -0300

    Alterações

--- produto.cs
+++ produto.cs
@@ -9,6 +9,34 @@ namespace Gamificacao3
 {
     public class Produto
     {
- 
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public decimal Preco { get; private set; }
+        public int QuantidadeEmEstoque { get; private set; }
+        public Produto(int id, string nome, string descricao, decimal preco, int quantidadeEmEstoque)
+        {
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+            throw new ArgumentException("Nome do produto não pode ser vazio ou nulo.");
+            }
+            if (string.IsNullOrWhiteSpace(descricao))
+            {
+            throw new ArgumentException("Descrição do produto não pode ser vazia ou nula.");
+            }
+            if (preco <= 0)
+            {
+            throw new ArgumentException("Preço do produto deve ser maior que zero.");
+            }
+            if (quantidadeEmEstoque < 0)
+            {
+            throw new ArgumentException("Quantidade em estoque não pode ser negativa.");
+            }
+            Id = id;
+            Nome = nome;
+            Descricao = descricao;
+            Preco = preco;
+            QuantidadeEmEstoque = quantidadeEmEstoque;
+        }
     }
 }
